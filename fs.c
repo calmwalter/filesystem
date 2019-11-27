fbe71ab0c3454ef545c9272d2485fe291e047a42
calmwalter <calmwalter@outlook.com>
2019-11-28 00:05:45 +0800

    implement the archtecture

--- fs.c
+++ fs.c
@@ -1,3 +1,49 @@
 #include<stdio.h>
 #include"fs.h"
 
+int write(char* file_path,char* content,filesystem* fs){}
+int read(char* file_path,filesystem* fs){}
+int ls(char* file_path,filesystem* fs){}
+int cp(char* file_path,filesystem* fs){}
+int paste(filesystem* fs){}
+int rm(char* file_path,filesystem* fs){}
+int mv(char* file_path, char* dest_file_path,filesystem* fs){}
+int mkdir(char* directory_name,filesystem* fs){}
+int find(char* name,filesystem* fs){}
+int cd(char* path,filesystem* fs){}
+
+
+int mount(char* disk_name,filesystem* fs){}
+int unmount(char* disk_name,filesystem* fs){}
+int create(char* disk_name,int size,filesystem* fs){}
+int format(char* disk_name,filesystem* fs){}
+int delete(char* disk_name,filesystem* fs){}
+
+
+int init(filesystem* fs){
+  //init the inner function of filesystem
+  fs->write = write;
+  fs->read = read;
+  fs->ls = ls;
+  fs->cp = cp;
+  fs->paste = paste;
+  fs->rm = rm;
+  fs->mv = mv;
+  fs->mkdir = mkdir;
+  fs->find = find;
+  fs->cd = cd;
+
+  fs->mount = mount;
+  fs->unmount = unmount;
+  fs->create = create;
+  fs->format = format;
+  fs->delete = delete;
+  
+  fs->init = init;
+
+  //init attributes
+  fs->disk_head=NULL;
+  fs->current_directory=-1;//means the directory is /dev
+  fs->current_disk
+}
+
--- fs.h
+++ fs.h
@@ -1,8 +1,16 @@
-#ifndef FS_H
-#define FS_H
+/*
+  all right reserved 2019 calmwalter
 
-int disk_size = 1024;//the size of the disk,unit kb
-int block_size = 4;//the size of the block,unit kb
+
+  we define the file structure like this:
+  /dev/disk_name/files or directory/..../files or directory
+
+  /dev is the root
+  /disk_name is the name of the disk
+ */
+
+#ifndef FS_H
+#define FILESYSTEM_H
 
 //type of the file
 #define TYPE_FILE 0
@@ -13,12 +21,11 @@ int block_size = 4;//the size of the block,unit kb
 #define FALSE 0
 
 //define number of direct pointer point to the data
-#define NUMBER_DIRECT_POINTER
+#define NUMBER_DIRECT_POINTER 5
 
 
 //store the main infomation of the disk
 typedef struct superblock{
-  int magic_number;//the number of the disk
   int inode_size;//the size of the file system
   int inode_number;
   int block_size;
@@ -33,27 +40,69 @@ typedef struct inode{
   int type;//type of the file
   int size;//size of the data
   int valid;//if the block has been used
-  int direct[5];//pointer point to the data directly
+  int direct[NUMBER_DIRECT_POINTER];//pointer point to the data directly
   int indirect;//point to block where store the pointer point to the data
 }inode;
 
-typedef struct fs
-{
-  int current_directory;//current directory number
-  int buffer;//buffer point to the inode
-  int (*write)(char*,char*);//write to the file(file_path,content)
-  int (*read)(char*);//read the file(file_path)
-  int (*ls)(char*);//list the files and directories in current directory(file_path)
-  int (*cp)(char*);//copy a file, put it to the buffer(file_path)
-  int (*paste)(char*);//paste the file in current buffer
-  int (*rm)(char*);//rm the file(file_path)
-  int (*mv)(char*,char *);//move a file to another place(file_path,dest_file_path)
-  int (*mkdir)(char*);//make a directory in current directory(directory_name)
-  int (*find)(char*);//find the file or directory under current directory or child directory recursively(file or directory name)
-  int (*init)();//init the file system, load the root directory
-  
-};
+//the disk that store the disk infomation:name,superblock,inode,and the next disk link
+typedef struct disk{
+  int number;
+  char* disk_name;//the name of the disk
+  superblock* sb;//the superblock infomation of the disk
+  inode* inodes;//the inodes in the disk
+}disk;
 
+typedef struct filesystem
+{
+  //attributes of filesystem
+  disk** disks;//dynamicly store the mounted disk
+  int current_directory;//current directory number
+  int current_disk;//the disk we current use
+  int buffer_inode,buffer_disk;//buffer_inode point to the inode number,buffer_disk point to the disk number
+  
+  //operation to access disk
+  int (*write)(char*,char*,struct filesystem*);//write to the file(file_path,content)
+  int (*read)(char*,struct filesystem*);//read the file(file_path)
+  int (*ls)(char*,struct filesystem*);//list the files and directories in current directory(file_path)
+  int (*cp)(char*,struct filesystem*);//copy a file, put it to the buffer(file_path)
+  int (*paste)(struct filesystem*);//paste the file in buffer to current directory
+  int (*rm)(char*,struct filesystem*);//rm the file(file_path)
+  int (*mv)(char*,char *,struct filesystem*);//move a file to another place(file_path,dest_file_path)
+  int (*mkdir)(char*,struct filesystem*);//make a directory in current directory(directory_name)
+  int (*find)(char*,struct filesystem*);//find the file or directory under current directory or child directory recursively(file or directory name)
+  int (*cd)(char*,struct filesystem*);//go to the directory
+  
+  //operation to manage disk
+  int (*mount)(char*,struct filesystem*);//mount the disk(disk_name)
+  int (*unmount)(char*,struct filesystem*);//unmount the disk(disk_name)
+  int (*create)(char*,int,struct filesystem*);//create the disk(disk_name,size)
+  int (*format)(char*,struct filesystem*);//format the disk(disk_name)
+  int (*delete)(char*,struct filesystem*);//delete the disk(disk_name)
+
+  //init the file system
+  int (*init)(struct filesystem*);//init the file system
+}filesystem;
+
+int write(char* file_path,char* content,filesystem* fs);
+int read(char* file_path,filesystem* fs);
+int ls(char* file_path,filesystem* fs);
+int cp(char* file_path,filesystem* fs);
+int paste(filesystem* fs);
+int rm(char* file_path,filesystem* fs);
+int mv(char* file_path, char* dest_file_path,filesystem* fs);
+int mkdir(char* directory_name,filesystem* fs);
+int find(char* name,filesystem* fs);
+int cd(char* path,filesystem* fs);
+
+
+int mount(char* disk_name,filesystem* fs);
+int unmount(char* disk_name,filesystem* fs);
+int create(char* disk_name,int size,filesystem* fs);
+int format(char* disk_name,filesystem* fs);
+int delete(char* disk_name,filesystem* fs);
+
+
+int init(filesystem* fs);
 
 
 #endif
